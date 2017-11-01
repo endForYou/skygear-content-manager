3d82b368f716dcefd0e40e56f5e9969d77229122
Kenji Pa <limouren@gmail.com>
2017-11-01 18:08:05 +0800

    Inject master key while proxying the request

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import requests
@@ -6,6 +7,8 @@ import skygear
 
 CMS_SKYGEAR_ENDPOINT = \
     os.environ.get('CMS_SKYGEAR_ENDPOINT', 'http://localhost:3000/')
+CMS_SKYGEAR_MASTER_KEY = \
+    os.environ.get('CMS_SKYGEAR_MASTER_KEY', 'FAKE_MASTER_KEY')
 
 HEADER_BLACKLIST = [
     'Access-Control-Allow-Credentials',
@@ -21,8 +24,8 @@ def api(request):
     resp = requests.request(
         request.method,
         url=CMS_SKYGEAR_ENDPOINT,
-        data=request.data,
-        headers=request.headers,
+        data=transform_request_data(request.data),
+        headers=transform_request_header(request.headers),
     )
 
     log_response(resp)
@@ -36,6 +39,31 @@ def api(request):
             if k not in HEADER_BLACKLIST
         ],
     )
+
+
+def transform_request_header(environ_headers):
+    # we are losing headers with multiple values here
+    headers = dict((k, v) for k, v in environ_headers)
+    headers['X-Skygear-Api-Key'] = CMS_SKYGEAR_MASTER_KEY
+
+    return headers
+
+
+def transform_request_data(data):
+    if not data:
+        return data
+
+    try:
+        json_body = json.loads(data.decode('utf-8'))
+    except json.decoder.JSONDecodeError:
+        return data
+
+    if not isinstance(json_body, dict):
+        return data
+
+    json_body['api_key'] = CMS_SKYGEAR_MASTER_KEY
+
+    return json.dumps(json_body).encode('utf-8')
 
 
 def log_request(request):
