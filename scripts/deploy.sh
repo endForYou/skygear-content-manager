329956820602f9f6cb61177bb309913a01aa4e88
Kenji Pa <limouren@gmail.com>
2017-11-02 14:33:01 +0800

    travis: update build scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,21 @@
 set -ex
 
-rm -rf 
+pushd .
+
+cd client
+
 npm install --production
 npm run build
 rm -rf public_html
 mv build public_html
 
+popd
+
+mv client/build public_html
+rm -rf client
+
 npm install -g skycli
 mkdir -p /home/travis/.skycli
 echo $SKYCLI_RC > $HOME/.skycli/skyclirc
-rm -rf node_modules
+
 skycli deploy
