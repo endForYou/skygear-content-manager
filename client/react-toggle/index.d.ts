38a5dbe79e1b6496ce05fad5002abb5797a8c792
Kenji Pa <limouren@gmail.com>
2017-11-07 23:13:12 +0800

    Provide definition file for react-toggle

---
+++ client/react-toggle/index.d.ts
@@ -0,0 +1,35 @@
+// Type definitions for react-toggle 4.0.2
+// Project: react-toggle
+// Definitions by: Kenji Pa <https://twitter.com/limouren>
+
+declare module 'react-toggle' {
+  import * as React from 'react';
+
+  export default class ReactToggle extends React.Component<ReactToggleProps> {}
+
+  export interface ReactToggleProps {
+    checked?: boolean;
+    defaultChecked?: boolean;
+    onChange?: React.ReactEventHandler<ReactToggleElement>;
+    onFocus?: React.ReactEventHandler<ReactToggleElement>;
+    onBlur?: React.ReactEventHandler<ReactToggleElement>;
+    name?: string;
+    value?: string;
+    id?: string;
+    icons?: IconsType;
+    'aria-labelledby'?: string;
+    'aria-label'?: string;
+    disabled?: boolean;
+  }
+
+  export interface ReactToggleElement {
+    checked: boolean;
+  }
+
+  export type IconsType =
+    | boolean
+    | {
+        checked: React.ReactNode;
+        unchecked: React.ReactNode;
+      };
+}
