1cd78be4fd0cf4298417cf1c6a074754664fe999
Joseph Chow <josephchow@oursky.com>
2018-01-02 18:45:21 +0800

    Initialize notification schema #17

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 from .skygear_content_manager import *
+from . import push_notifications
---
+++ push_notifications/__init__.py
@@ -0,0 +1,22 @@
+import skygear
+from skygear.utils import db
+
+
+@skygear.event("before-plugins-ready")
+def cms_push_notification_db_init(config):
+    with db.conn() as conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS _cms_push_campaign (
+                id text NOT NULL,
+                device text NOT NULL,
+                content text NOT NULL,
+                send_time timestamp NOT NULL,
+                user_id text NOT NULL,
+                PRIMARY KEY (id)
+            );
+        """)
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS _cms_version (
+                version_num text NOT NULL
+            );
+        """)
