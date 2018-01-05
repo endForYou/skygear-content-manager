9b658e48747615e6287dc7c7a6877d57b148e49a
Percy Lam <lam_percy@hotmail.com>
2018-01-05 15:22:39 +0800

    only allow number for input of integer filter

--- client/src/components/FilterList.tsx
+++ client/src/components/FilterList.tsx
@@ -12,7 +12,7 @@ interface FilterListProps {
 export class FilterList extends React.PureComponent<FilterListProps> {
 
   public renderFilter(filter: Filter) {
-    const { handleFilterValueChange, onCloseFilterClicked } = this.props;
+    const { onCloseFilterClicked } = this.props;
     return (
       <div key={filter.id} className="form-inline mb-2">
         <div className="form-group mr-2">
@@ -22,13 +22,7 @@ export class FilterList extends React.PureComponent<FilterListProps> {
           {this.renderFilterSelect(filter)}
         </div>
         <div className="form-group mr-4">
-          <input 
-            type="text"
-            className="form-control"
-            autoFocus={true}
-            onChange={event => handleFilterValueChange(filter, event)}
-            value={filter.value}
-          />
+          {this.renderInput(filter)}
         </div>
         <div className="form-group">
           <button onClick={() => onCloseFilterClicked(filter)} type="button" className="close">
@@ -78,6 +72,43 @@ export class FilterList extends React.PureComponent<FilterListProps> {
         <option value={IntegerFilterQueryType.LessThanOrEqualTo}>Less than or equal to</option>
         <option value={IntegerFilterQueryType.GreaterThanOrEqualTo}>Greater than or equal to</option>
       </select>);
+  }
+
+  public renderInput(filter: Filter) {
+    switch (filter.type) {
+      case FilterType.StringFilterType:
+        return this.renderStringInput(filter);
+      case FilterType.IntegerFilterType:
+        return this.renderIntegerInput(filter);
+    }
+  }
+
+  public renderStringInput(filter: Filter) {
+    const { handleFilterValueChange } = this.props;
+
+    return (
+      <input 
+        type="text"
+        className="form-control"
+        autoFocus={true}
+        onChange={event => handleFilterValueChange(filter, event)}
+        value={filter.value}
+      />
+    );
+  }
+
+  public renderIntegerInput(filter: Filter) {
+    const { handleFilterValueChange } = this.props;
+
+    return (
+      <input 
+        type="number"
+        className="form-control"
+        autoFocus={true}
+        onChange={event => handleFilterValueChange(filter, event)}
+        value={filter.value}
+      />
+    );
   }
 
   public render() {
