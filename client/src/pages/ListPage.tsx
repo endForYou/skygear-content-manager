e0424a0d484432c25d4d84825afb8a17071d6ae3
Steven Chan <stevenchan@oursky.com>
2018-02-21 16:36:02 +0800

    Add action buttons to list page, which call export api
    Export API require cms access token in url query string

--- client/src/pages/ListPage.tsx
+++ client/src/pages/ListPage.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Dispatch } from 'redux';
-import { Record } from 'skygear';
+import skygear, { Record } from 'skygear';
 
 import { RecordActionDispatcher } from '../actions/record';
 import {
