3cdca585cce0a5695e492e7e61e58f846f930f5f
Steven Chan <stevenchan@oursky.com>
2018-06-11 18:48:49 +0800

    Raise exception when create imported_file if duplicated file name found

--- skygear_content_manager/file_import/lambdas.py
+++ skygear_content_manager/file_import/lambdas.py
@@ -1,4 +1,5 @@
 import skygear
+from sqlalchemy.exc import IntegrityError
 
 from marshmallow import Schema, fields
 from skygear.asset import get_signer
@@ -59,6 +60,7 @@ def register_lambda(settings):
             return {'errors': errors}
 
         with scoped_session() as session:
+            ensure_unique_file_name(session, new_imported_files)
             imported_files = []
             for file in new_imported_files:
                 file = CmsImportedFile.from_dict(file)
@@ -77,3 +79,20 @@ def inject_signed_url(files):
     signer = get_signer()
     for file in files:
         file['url'] = signer.sign(file['asset'])
+
+
+def ensure_unique_file_name(session, files):
+    duplicated_files = session.query(CmsImportedFile) \
+        .filter(CmsImportedFile.id.in_([f['id'] for f in files])) \
+        .all()
+    duplicated_names = [f.id for f in duplicated_files]
+
+    if len(duplicated_names) > 0:
+        raise DuplicatedFileException(duplicated_names)
+
+
+class DuplicatedFileException(Exception):
+
+    def __init__(self, duplicated_names):
+        message = 'File name ({}) existed already.'.format(', '.join(duplicated_names))
+        super(DuplicatedFileException, self).__init__(message)
