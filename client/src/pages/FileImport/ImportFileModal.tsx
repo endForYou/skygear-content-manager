eaafe01e27e09b26584d171ea60c9102a5952b7f
Steven Chan <stevenchan@oursky.com>
2018-06-11 15:08:17 +0800

    Add file input in import file modal body
    Allow add and remove files, files with duplicated name are replaced.

--- client/src/pages/FileImport/ImportFileModal.tsx
+++ client/src/pages/FileImport/ImportFileModal.tsx
@@ -1,4 +1,7 @@
+import './ImportFileModal.scss';
+
 import * as React from 'react';
+import Dropzone, { DropFileEventHandler } from 'react-dropzone';
 
 import { Modal } from '../../components/Modal';
 
@@ -7,15 +10,28 @@ interface Props {
   onDismiss: () => void;
 }
 
-export class ImportFileModal extends React.PureComponent<Props> {
+interface State {
+  files: File[];
+}
+
+export class ImportFileModal extends React.PureComponent<Props, State> {
+  private fileInput: HTMLInputElement | null = null;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      files: [],
+    };
+  }
+
   public render() {
-    const { onDismiss, show } = this.props;
+    const { show } = this.props;
 
     return (
       <Modal
         show={show}
         title="Import Files"
-        onDismiss={onDismiss}
+        onDismiss={this.onDismiss}
         body={this.renderBody}
         footer={() => [
           <a
@@ -32,7 +48,7 @@ export class ImportFileModal extends React.PureComponent<Props> {
             href="#"
             role="button"
             className="modal-button-secondary"
-            onClick={onDismiss}
+            onClick={this.onDismiss}
           >
             Cancel
           </a>,
@@ -41,16 +57,110 @@ export class ImportFileModal extends React.PureComponent<Props> {
     );
   }
 
-  // TODO: render file upload
   private renderBody = () => {
+    const { files } = this.state;
+
     return (
-      <div>
-        Select files.
+      <div className="file-import-modal">
+        <button type="button" className="" onClick={this.removeAllFiles}>
+          Clear all
+        </button>
+        <input
+          ref={ref => (this.fileInput = ref)}
+          type="file"
+          onChange={this.onFileInputChange}
+        />
+        <Dropzone
+          className="file-dropzone"
+          disablePreview={true}
+          // TODO: allow customization
+          // accept="*"
+          multiple={true}
+          onDropAccepted={this.onDropAccepted}
+        >
+          {files.length === 0 && <div className="file-empty">Empty files.</div>}
+          <div className="file-list">
+            {files.map((f, index) => (
+              <div
+                key={index}
+                className="file-item"
+                onClick={evt => evt.stopPropagation()}
+              >
+                <div className="file-name">{f.name}</div>
+                <button
+                  type="button"
+                  className="file-remove close"
+                  aria-label="Close"
+                  onClick={evt => {
+                    evt.stopPropagation();
+                    this.removeFile(f);
+                  }}
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            ))}
+          </div>
+        </Dropzone>
       </div>
     );
-  }
+  };
+
+  private onDismiss = () => {
+    const { onDismiss } = this.props;
+    this.removeAllFiles();
+    onDismiss();
+  };
+
+  private onDropAccepted: DropFileEventHandler = (accepted: File[]) => {
+    this.addFiles(accepted);
+  };
+
+  private onFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    if (event.target.files) {
+      const filelist = event.target.files;
+      let files: File[] = [];
+      for (let index = 0; index < filelist.length; index++) {
+        const file = filelist.item(index);
+        if (file == null) {
+          continue;
+        }
+
+        files = [...files, file];
+      }
+      this.addFiles(files);
+
+      if (this.fileInput) {
+        this.fileInput.value = '';
+      }
+    }
+  };
+
+  private addFiles = (files: File[]) => {
+    this.setState({
+      files: [...this.state.files, ...files],
+    });
+  };
+
+  private removeFile = (file: File) => {
+    const filesToRemove = this.state.files.filter(f => f.name === file.name);
+    if (filesToRemove.length === 0) {
+      return;
+    }
+
+    this.setState({
+      files: this.state.files.filter(f => f.name !== file.name),
+    });
+  };
+
+  private removeAllFiles = () => {
+    this.setState({
+      files: [],
+    });
+  };
 
   private onImportClick = () => {
     console.log('import click');
-  }
+  };
 }
