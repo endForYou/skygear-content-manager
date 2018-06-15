0c1ece90dd47cd034a97a8e93f0b8b6d0d93dc3b
Steven Chan <stevenchan@oursky.com>
2018-06-15 13:06:13 +0800

    Add timezone picker to settings page

--- client/src/pages/SettingsPage.tsx
+++ client/src/pages/SettingsPage.tsx
@@ -1,7 +1,89 @@
-import * as React from 'react';
+import './SettingsPage.scss';
 
-export class SettingsPage extends React.PureComponent {
-  public render() {
-    return <div />;
+import moment from 'moment-timezone';
+import * as React from 'react';
+import Select, { Option, OptionValues } from 'react-select';
+
+// tslint:disable-next-line:no-empty-interface
+interface Props {}
+
+interface State {
+  timezone: string;
+}
+
+interface TzPickerProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const timezoneOptions = moment.tz.names().map(n => ({
+  label: n,
+  value: n,
+}));
+
+const TzPicker: React.SFC<TzPickerProps> = ({ value, onChange }) => {
+  const handleTimezoneChange = (option: Option<OptionValues> | null) => {
+    if (option == null) {
+      return;
+    }
+
+    if (typeof option.value !== 'string') {
+      return;
+    }
+
+    onChange(option.value);
+  };
+
+  return (
+    <Select
+      className="dropdown-select"
+      name={name}
+      clearable={false}
+      searchable={true}
+      placeholder=""
+      value={value}
+      onChange={handleTimezoneChange}
+      options={timezoneOptions}
+    />
+  );
+};
+
+export class SettingsPage extends React.PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      timezone: moment.tz.guess(),
+    };
   }
+
+  public render() {
+    return (
+      <div className="settings-page">
+        <div className="topbar">
+          <div className="title">Settings</div>
+        </div>
+
+        <div className="form-groups">
+          <div className="form-group">
+            <div className="form-label">
+              <label htmlFor="time-zone">Time zone</label>
+            </div>
+            <div className="form-field">
+              <TzPicker
+                value={this.state.timezone}
+                onChange={this.handleTimezoneChange}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  private handleTimezoneChange = (value: string) => {
+    this.setState({
+      timezone: value,
+    });
+  };
 }
