780cbc1c6fcf49e6d4fdfc1252b2c0452a78d03f
Louis Chan <louischan@oursky.com>
2019-04-08 19:07:19 +0800

    Support default value in embedded field

--- client/src/fields/EmbeddedBackReferenceField.tsx
+++ client/src/fields/EmbeddedBackReferenceField.tsx
@@ -9,6 +9,7 @@ import {
   EmbeddedBackReferenceListFieldConfig,
   FieldConfig,
   SortOrder,
+  isFieldEditable,
 } from '../cmsConfig';
 import { Arrow, ArrowDirection } from '../components/Arrow';
 import { PrimaryButton } from '../components/PrimaryButton';
@@ -344,6 +345,12 @@ interface FieldProps {
 
 function FormGroup(props: FieldProps): JSX.Element {
   const { fieldConfig, onFieldChange, record, validationError } = props;
+  const fieldName = fieldConfig.name;
+  const value = Object.prototype.hasOwnProperty.call(record, fieldName)
+    ? record[fieldName]
+    : isFieldEditable(fieldConfig)
+    ? fieldConfig.defaultValue
+    : undefined;
   return (
     <div className="record-form-group">
       <label
@@ -357,7 +364,7 @@ function FormGroup(props: FieldProps): JSX.Element {
       <Field
         className="record-form-field"
         config={fieldConfig}
-        value={record[fieldConfig.name]}
+        value={value}
         context={FieldContext(record)}
         onFieldChange={onFieldChange}
         validationError={validationError}
